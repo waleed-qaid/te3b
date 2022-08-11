fcb79c4ea049dd736bccb2eea24f20f8bf705325
waleed-qaid <48764757+waleed-qaid@users.noreply.github.com>
2022-08-11 09:14:37 +0300

    Update ‏‏www.1des.net-chatt-one-design.js

--- ‏‏www.1des.net-chatt-one-design.js
+++ ‏‏www.1des.net-chatt-one-design.js
@@ -1,29 +1,28 @@
-
 $("style").last().append(`
-    .one_4 {background-image: linear-gradient(-225deg, #0000 0%, #0000 29%, #a6a6a629 67%, #fff 100%);
+#users.one_1 .inroom.uzr {background-image: linear-gradient(-225deg, #0000 0%, #0000 29%, #a6a6a629 67%, #fff 100%);
     border-radius: 5px;
     border: 1px solid #fffefe;
       border-bottom-color: rgb(255, 254, 254);
       border-bottom-style: solid;
       border-bottom-width: 1px;
     }
-    .one_5 {width: 10px!important;
+#users.one_1 .inroom.uzr img.ustat {width: 10px!important;
         height: 10px;
         border-radius: 50%;
     }
-    .one_6 {border: 1px solid white;
+#users.one_1 .inroom.uzr .fitimg.u-pic {border: 1px solid white;
     margin-top: 2px;
     border-radius: 5px;
     box-shadow: inset 0 0px 0px rgba(0, 0, 0, 0.08), 0 0px 2px #ff0060;
     }
-    .one_7 {text-align: center;
+#users.one_1 .inroom.uzr .u-msg {text-align: center;
     background-image: -webkit-linear-gradient(75deg,rgb(0 0 0 / 53%) 25%,#ff0066ab 25%,#000000 50%,#c90051b8 50%,#ff006600 75%);
     background-size: 200% auto;
     background-size: 200% auto;
     -webkit-background-clip: text;
     -webkit-text-fill-color: #0000;
     }
-    .one_8 {text-transform: uppercase;
+#users.one_1 .inroom.uzr span.uhash {text-transform: uppercase;
     background-image: linear-gradient(-225deg, #7b7b7b 0%, #737373 29%, #ff1361 67%, #3600ff 100%);
     background-size: 220% auto;
     -webkit-background-clip: text;
@@ -31,30 +30,20 @@ $("style").last().append(`
     animation: textclip 2s linear infinite;
     display: inline-block;
     }
-    .one_9 {transform: rotate(20deg);
+#users.one_1 .inroom.uzr img.co {transform: rotate(20deg);
     margin-top: 2px !important;
     }`);
 var room_set = false;
 setInterval(function () {
-    if (myroom == '180b31492f9x922d0131xn7fcg1') {
+    if (myroom == '18287f41f1dxa01b60e6xdn9dg8') {
         if (room_set == false) {
             room_set = true;
-			$('#users div.inroom').addClass('one_4');
-			$('#users div.inroom img.ustat').addClass('one_5');
-			$('#users div.inroom .fitimg.u-pic').addClass('one_6');
-			$('#users div.inroom .mini.u-msg').addClass('one_7');
-			$('#users div.inroom span.uhash').addClass('one_8');
-            $('#users div.inroom img.co').addClass('one_9');
+			$('#users').addClass('one_1');
         }
     } else {
         if (room_set == true) {
             room_set = false;
-			$('#users div.inroom').removeClass('one_4');
-			$('#users div.inroom img.ustat').removeClass('one_5');
-			$('#users div.inroom .fitimg.u-pic').removeClass('one_6');
-			$('#users div.inroom .mini.u-msg').removeClass('one_7');
-			$('#users div.inroom span.uhash').removeClass('one_8');
-            $('#users div.inroom img.co').removeClass('one_9'); 
+			$('#users').removeClass('one_7');
         } 
     } 
 }, 1000);
