5db13001278df9c6b40b8efcc0f00715f10b4eb5
waleed-qaid <48764757+waleed-qaid@users.noreply.github.com>
2022-09-27 05:19:54 +0300

    Update ‏‏www.1des.net-chatt-one-design-t.js

--- ‏‏www.1des.net-chatt-one-design-t.js
+++ ‏‏www.1des.net-chatt-one-design-t.js
@@ -145,7 +145,7 @@ display: flex !important;
 `);
 var r_one = false;
 setInterval(function () {
-    if (myroom == '18288790f64x23ec4a42x-jg5xl3') {
+    if (myroom == '180b31492f9x922d0131xn7fcg1') {
         if (r_one == false) {
             r_one = true;
 		$('#rdark_mod').addClass('one4');
@@ -186,7 +186,7 @@ height:10px;border-radius:50%
 #d2.one2 .uzr .fitimg.u-pic,#users.one1 .inroom.uzr .fitimg.u-pic{border:1px solid #fff;margin-top:2px;border-radius:5px;box-shadow:inset 0 0 0 rgba(0,0,0,.08),0 0 2px #ff0060}#users.one1 .inroom.uzr .u-msg{text-align:center;background-image:-webkit-linear-gradient(75deg,rgb(0 0 0 / 53%) 25%,#ff0066ab 25%,#000 50%,#c90051b8 50%,#ff006600 75%);background-size:200% auto;-webkit-background-clip:text;-webkit-text-fill-color:#0000}#users.one1 .inroom.uzr span.uhash{text-transform:uppercase;background-image:linear-gradient(-225deg,#7b7b7b 0,#737373 29%,#ff1361 67%,#3600ff 100%);background-size:220% auto;-webkit-background-clip:text;-webkit-text-fill-color:transparent;animation:2s linear infinite textclip;display:inline-block}#users.one1 .inroom.uzr img.co{transform:rotate(20deg);margin-top:2px!important}#d2.one2 .uzr.d-flex.mm{background-size: auto;border-radius:5px;border:1px solid #fffefe6b;border-bottom:1px solid #fffefe00}`);
 var r_one = false;
 setInterval(function () {
-    if (myroom == '18288790f64x23ec4a42x-jg5xl3') {
+    if (myroom == '180b31492f9x922d0131xn7fcg1') {
         if (r_one == false) {
             r_one = true;
 		$('#users').addClass('one1');
