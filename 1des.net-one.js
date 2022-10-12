1fd82361017b2a6d8bc1af0e0eb6272d1d16b2f5
waleed-qaid <48764757+waleed-qaid@users.noreply.github.com>
2022-10-12 19:29:44 +0300

    Update 1des.net-one.js

--- 1des.net-one.js
+++ 1des.net-one.js
@@ -361,7 +361,7 @@ var itv3 = setInterval(function () {
         $("#movon").append('<div style="max-width: 340px; min-width: 300px; color:white; width: 338px; float: right;" class="modal-header label-primary"><span class="pull-right clickable badge"><i class="fa fa-times"></i></span><label style="margin:1px; float: right;" > My vedio </label></div>');
           
 		  
-        $("#movon").append('<iframe id="ifr_mov2" style="max-width: 340px;min-width: 300px;right: 0; top: 35px; left: auto;" class="modal in" role="dialog" style="z-index: 2100; display: none; style="max-width:240px;" src="https://www.youtube.com/embed/fpnPfQ1bQ3o" allowfullscreen="" width="338" height="490" frameborder="0"></iframe>');
+        $("#movon").append('<iframe id="ifr_mov2" style="max-width: 340px;min-width: 300px;right: 0; top: 35px; left: auto;" class="modal in" role="dialog" style="z-index: 2100; display: none; style="max-width:240px;" src="https://www.youtube.com/embed/J2IBXCCCnKg" allowfullscreen="" width="338" height="490" frameborder="0"></iframe>');
         
        $("#ifr_mov2").show(function(){
         $("#upro").modal("hide");
