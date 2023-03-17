95022802885c0d9675b3f5d3b6f73845226d0fc9
waleed-qaid <48764757+waleed-qaid@users.noreply.github.com>
2023-03-17 09:32:55 +0300

    Update ‏‏www.1des.net-chatt-one-design-t.js

--- ‏‏www.1des.net-chatt-one-design-t.js
+++ ‏‏www.1des.net-chatt-one-design-t.js
@@ -1516,7 +1516,7 @@ box-shadow: inset 0 0 0 rgba(0, 0, 0, 0.08), 0px 0 5px #fff;
 border-radius: 15px;
 }
 .nosel.d-flex.room.r186a0eb1751x05bc9ec5-x-mvjfyj {
-    background-image: url(https://up6.cc/2023/03/167859061401921.gif);
+    background-image: url(https://up6.cc/2023/03/167903469877061.gif);
     background-size: cover;
     border-radius: 15px 0px 15px 0px;
     display: inline-block;
